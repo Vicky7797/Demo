09828b6cab947027b12736197c13ce9fc20c9982
Vicky7797 <vicky.nct@gmail.com>
2020-11-25 15:18:42 -0500

    Commit

---
+++ src/main/test/com/gradle/test/TestClass.java
@@ -0,0 +1,14 @@
+package com.gradle.test;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+		System.out.println("Test Working");
+
+	}
+
+}
