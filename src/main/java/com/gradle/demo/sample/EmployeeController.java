5385584e2a9f2bc9fa44ab3b49b8f1201fe8f468
Vignesh Duraisamy <Vignesh_Duraisamy@ad.infosys.com>
2020-11-25 14:43:57 -0500

    Commit

--- src/main/java/com/gradle/demo/sample/EmployeeController.java
+++ src/main/java/com/gradle/demo/sample/EmployeeController.java
@@ -1,17 +1,9 @@
 package com.gradle.demo.sample;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.gradle.demo.sample.model.Employee;
 
 /**
  * Handles requests for the Employee service.
@@ -19,19 +11,10 @@ import com.gradle.demo.sample.model.Employee;
 @Controller
 public class EmployeeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
-
-	// Map to store employees, ideally we should use database
-	Map<Integer, Employee> empData = new HashMap<Integer, Employee>();
-
 	@RequestMapping(value = "/api/sample", method = RequestMethod.GET)
-	public @ResponseBody Employee getDummyEmployee() {
-		logger.info("Start getDummyEmployee");
-		Employee emp = new Employee();
-		emp.setId(9999);
-		emp.setName("Dummy");
-		emp.setCreatedDate(new Date());
-		empData.put(9999, emp);
-		return emp;
+	public @ResponseBody void getDummyEmployee() {
+
+		System.out.println("");
+
 	}
 }
